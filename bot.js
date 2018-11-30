e1863b5c8e29cd9f80da978bed83c477d5d9e080
wented6 <44713416+wented6@users.noreply.github.com>
2018-11-30 17:39:02 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,15 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 client.on('ready', () => {
-    client.user.setGame(مؤخرتكَ مكانَ جيد لوضعْ رآيك فيهاَ.,'https://www.twitch.tv/f9u9l1i%27)
-
+   console.log(`----------------`);
+      console.log(`Desert Bot- Script By : Diamond Codes`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Ween, ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`مؤخرتكَ مكانَ جيد لوضعْ رآيك فيهاَ.`,"https://www.twitch.tv/xkilleryt")
+client.user.setStatus("online")
+ 
 });
+
 client.login(process.env.BOT_TOKEN);
