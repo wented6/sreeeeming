d1731fd6112db4c587b8177c3845b59942299765
wented6 <44713416+wented6@users.noreply.github.com>
2018-11-30 19:36:05 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,83 +5,18 @@ const client = new Discord.Client();
 
 
 
-client.on('ready', () => {
-   console.log(----------------);
-      console.log(Desert Bot- Script By : Diamond Codes);
-        console.log(----------------);
-      console.log(ON ${client.guilds.size} Ween, ');
-    console.log(----------------);
-  console.log(Logged in as ${client.user.tag}!);
-client.user.setGame(Universe server.,"https://www.twitch.tv/xkilleryt%22)
-client.user.setStatus("online")
- 
-});
-
 
 client1.on('ready', () => {
-   console.log(----------------);
-      console.log(Desert Bot- Script By : Diamond Codes);
-        console.log(----------------);
-      console.log(ON ${client.guilds.size} Ween, ');
-    console.log(----------------);
-  console.log(Logged in as ${client.user.tag}!);
-client.user.setGame(Universe server.,"https://www.twitch.tv/xkilleryt%22)
-client.user.setStatus("online")
- 
+    client.user.setGame(`Universe server.`,'https://www.twitch.tv/f9u9l1i')
+    
 });
+
 
 client2.on('ready', () => {
-   console.log(----------------);
-      console.log(Desert Bot- Script By : Diamond Codes);
-        console.log(----------------);
-      console.log(ON ${client.guilds.size} Ween, ');
-    console.log(----------------);
-  console.log(Logged in as ${client.user.tag}!);
-client.user.setGame(Universe server.,"https://www.twitch.tv/xkilleryt%22)
-client.user.setStatus("online")
- 
+    client.user.setGame(`Universe server.`,'https://www.twitch.tv/f9u9l1i')
+    
 });
 
-
-client3.on('ready', () => {
-   console.log(----------------);
-      console.log(Desert Bot- Script By : Diamond Codes);
-        console.log(----------------);
-      console.log(ON ${client.guilds.size} Ween, ');
-    console.log(----------------);
-  console.log(Logged in as ${client.user.tag}!);
-client.user.setGame(Universe server.,"https://www.twitch.tv/xkilleryt%22)
-client.user.setStatus("online")
- 
-});
-
-
-
-client4.on('ready', () => {
-   console.log(----------------);
-      console.log(Desert Bot- Script By : Diamond Codes);
-        console.log(----------------);
-      console.log(ON ${client.guilds.size} Ween, ');
-    console.log(----------------);
-  console.log(Logged in as ${client.user.tag}!);
-client.user.setGame(Universe server.,"https://www.twitch.tv/xkilleryt%22)
-client.user.setStatus("online")
- 
-});
-
-
-
-client5.on('ready', () => {
-   console.log(----------------);
-      console.log(Desert Bot- Script By : Diamond Codes);
-        console.log(----------------);
-      console.log(ON ${client.guilds.size} Ween, ');
-    console.log(----------------);
-  console.log(Logged in as ${client.user.tag}!);
-client.user.setGame(Universe server.,"https://www.twitch.tv/xkilleryt%22)
-client.user.setStatus("online")
- 
-});
 
 
 
@@ -95,6 +30,3 @@ client.user.setStatus("online")
 
 client1.login(process.env.BOT_TOKEN);
 client2.login(process.env.BOT_TOKEN);
-client3.login(process.env.BOT_TOKEN);
-client4.login(process.env.BOT_TOKEN);
-client5.login(process.env.BOT_TOKEN);
